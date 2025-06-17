cdf3f55d60dd22bdf6713dce7862e383322aac05
lKinox <97477925+lKinox@users.noreply.github.com>
2025-06-17 00:27:33 -0400

    feat/Changes

--- src/components/layout/code-editor.tsx
+++ src/components/layout/code-editor.tsx
@@ -86,7 +86,7 @@ const usuario = "Desarrollador";`,
         <div
             className={`
                 w-full 
-                max-w-[700px]
+                max-w-[600px]
                 mx-auto 
                 rounded 
                 overflow-hidden 
